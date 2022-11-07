568411fca6b46ed9dfd2936e9d6eb2cef5119586
lucyoz <lucyoz.kwon@gmail.com>
2022-11-07 12:27:17 +0900

    first commit

---
+++ src/main/java/com/example/springbootmustachebbs/SpringbootMustacheBbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmustachebbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustacheBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMustacheBbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootmustachebbs/SpringbootMustacheBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmustachebbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMustacheBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
