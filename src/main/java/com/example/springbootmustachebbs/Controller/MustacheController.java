e1135662baef000f63a71512e568cce8146dc446
lucyoz <lucyoz.kwon@gmail.com>
2022-11-07 13:06:09 +0900

    Controller, mustache 실습1

---
+++ src/main/java/com/example/springbootmustachebbs/Controller/MustacheController.java
@@ -0,0 +1,24 @@
+package com.example.springbootmustachebbs.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MustacheController {
+
+    @GetMapping(value="/hi")
+    public String mustacheCon(Model model){
+        model.addAttribute("username","rok");   //view에 값 넘기기
+        return "greetings"; //greetings라는 이름의 view를 리턴
+    }
+
+    @GetMapping(value="/hi/{id}")
+    public String mustacheCon2(@PathVariable String id, Model model){
+        model.addAttribute("username","rok");
+        model.addAttribute("id",id);
+        return "greetings";
+    }
+
+}
